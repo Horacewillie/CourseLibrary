c73fd91a415824062aba718c6c449e746fcdaf15
horace saturday <horacewillie7@gmail.com>
2021-12-22 22:28:54 +0100

    Add project files.

---
+++ CourseLibrary.API/Entities/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.Entities
+{
+    public class Author
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string  FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTimeOffset DateOfBirth { get; set; }
+
+        public string MainCategory { get; set; }
+        public ICollection<Course> Courses { get; set; } = new List<Course>();
+
+    }
+}
